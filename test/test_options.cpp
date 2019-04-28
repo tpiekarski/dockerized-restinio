d4b674381ea40fc7b89161871243399a689923a5
Thomas Piekarski <t.piekarski@deloquencia.de>
2019-04-28 10:31:32 +0200

    Adding Unit Test for parsing Command Line Options

--- test/test_options.cpp
+++ test/test_options.cpp
@@ -8,6 +8,7 @@
 #define BOOST_TEST_MODULE "TestOptions"
 
 #include "options.h"
+#include <vector>
 #include <boost/test/unit_test.hpp>
 
 using namespace dockerized_restinio;
@@ -17,4 +18,19 @@ BOOST_AUTO_TEST_CASE(test_default_options) {
 
   BOOST_CHECK_EQUAL(options.address(), "0.0.0.0");
   BOOST_CHECK_EQUAL(options.port(), 8080);
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_options) {
+  std::vector<char*> argv;
+  argv.push_back((char*) "test");
+  argv.push_back((char*) "--port");
+  argv.push_back((char*) "8085");
+  argv.push_back((char*) "--address");
+  argv.push_back((char*) "127.0.0.1");
+
+  Options options;
+  options.parse(argv.size(), argv.data());
+
+  BOOST_CHECK_EQUAL(options.address(), "127.0.0.1");
+  BOOST_CHECK_EQUAL(options.port(), 8085);
 }
